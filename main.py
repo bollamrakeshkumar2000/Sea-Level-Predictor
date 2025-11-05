69bcd067fd83939888b7582a147a2daa93766834
bollamrakeshkumar2000 <bollamrakeshkumar2000@gmail.com>
2025-11-05 12:32:33 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import sea_level_predictor
+
+# Generate the sea level plot
+print("Generating sea level prediction plot...")
+fig = sea_level_predictor.draw_plot()
+print("Plot saved as sea_level_plot.png")
+
+# Run unit tests (if available)
+# from unittest import main
+# main(module='test_module', exit=False)
